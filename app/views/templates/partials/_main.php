a2e2dfbd2a30d86a67f59d226e55f85bb297e75d
Majid <Ajouaoujamid@gmail.com>
2024-09-30 14:20:05 +0200

    Structure de l'index et des partials

---
+++ app/views/templates/partials/_main.php
@@ -0,0 +1,42 @@
+<div class="row">
+          <!-- About Me (Left Sidebar) Start -->
+          <div class="col-md-3">
+          <?php include "../app/views/templates/partials/_aside.php"; ?>
+          </div>
+          <!-- About Me (Left Sidebar) End -->
+
+          <!-- Blog Post (Right Sidebar) Start -->
+          <div class="col-md-9">
+            <div class="col-md-12 page-body">
+              <div class="row">
+                <div class="col-md-12 content-page">
+                  <!-- ADD A POST -->
+                  <div>
+                    <a href="form.html" type="button" class="btn btn-primary"
+                      >Add a Post</a
+                    >
+                  </div>
+                  <!-- ADD A POST END -->
+
+                  <!-- Blog Post Start -->
+                    <?php echo $content ?>
+                  <!-- Blog Post End -->
+
+                  <nav aria-label="Page navigation example" style="text-align: center;">
+                  <?php include "../app/views/templates/partials/_nav.php"; ?>
+                  </nav>
+
+                </div>
+              </div>
+            </div>
+
+            <!-- Footer Start -->
+            <div class="col-md-12 page-body margin-top-50 footer">
+              <footer>
+              <?php include "../app/views/templates/partials/_footer.php"; ?>
+              </footer>
+            </div>
+            <!-- Footer End -->
+          </div>
+          <!-- Blog Post (Right Sidebar) End -->
+        </div>
